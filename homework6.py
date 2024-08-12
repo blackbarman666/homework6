4b394638ddeaf1dae1eabbd8a1423a2d8c42c804
blackbarman666 <mihaman05@gmail.com>
2024-08-12 21:17:15 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+immutable_var = 10, 'яблок'
+print(immutable_var)
+#immutable_var_[0]= 'красных'
+#print(immutable_var)
+
+
+mutable_list =[ 1, 2 , 'pc' ,'hdd']
+print(mutable_list)
+mutable_list[0]='monik'
+print(mutable_list)
+
+
+
+
+
